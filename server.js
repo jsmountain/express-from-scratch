e0bdb9c23047895eeabe1b97789eb520a5fcf05a
SenimanMalam001 <elfinsanjaya12@gmail.com>
2018-05-17 12:46:08 +0700

    express-from-scrarch selesai

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 //setup view engine/
 app.set('views', __dirname+'/views/')
 app.set('view engine', 'ejs')
+
+//body parser
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 
 //membuar folder static public
 app.use(express.static('public'))
@@ -12,10 +18,25 @@ app.get('/', (req, res) => {
   res.render('main/index');
 });
 
-app.get('/kontak', (req, res) => {
+
+app.get('/post', (req, res) => {
   res.render('main/kontak');
 });
 
+app.post('/post', (req, res) => {
+	console.log(req.body)
+  res.render('main/succes', {
+  	data: req.body
+  });
+});
+
+/*app.get('/echo',(req, res) => {
+	res.render('post')
+})
+
+app.post('/echo',(req, res) => {
+	res.render('post')
+})*/
 //app.get('/', (req, res) => res.send('Hello Express!'))
 //app.get('/about', (req, res) => res.send('Express is Express'))
 app.listen(5432, () => console.log('Server Success berjalan di http://localhost:5432'))
