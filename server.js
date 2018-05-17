beb4a6e06e8497743ae1f9e0917d890ad3dfb6c5
SenimanMalam001 <elfinsanjaya12@gmail.com>
2018-05-17 09:12:54 +0700

    express-ejs membuat home dan kontak part2

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const app = express()
 app.set('views', __dirname+'/views/')
 app.set('view engine', 'ejs')
 
+//membuar folder static public
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('main/index');
-
 });
 
+app.get('/kontak', (req, res) => {
+  res.render('main/kontak');
+});
 
 //app.get('/', (req, res) => res.send('Hello Express!'))
 //app.get('/about', (req, res) => res.send('Express is Express'))
-app.listen(5432, () => console.log('Hello World app berjalan di http://localhost:5432'))
+app.listen(5432, () => console.log('Server Success berjalan di http://localhost:5432'))
